56c4a60284e9e669e02b1fcb4ec8da02de88e405
unknown <attiamahmoud222@gmail.com>
2022-09-07 12:06:31 +0200

    fix some bugs

--- app.js
+++ app.js
@@ -6,8 +6,24 @@ const connectToDb = require("./db/connect-db");
 const tasksRouter = require("./routes/tasks");
 const authRouter = require("./routes/auth");
 const authMiddlerware = require("./middleware/authMiddleware");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
 const app = express();
 app.use(express.json());
+app.set("trust proxy", 1);
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+);
+
+app.use(helmet());
+app.use(xss());
+app.use(cors());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/tasks", authMiddlerware, tasksRouter);
