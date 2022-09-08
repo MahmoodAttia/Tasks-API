d501f2080bd6d3cebd0f5d276cda7e46a3f250b2
unknown <attiamahmoud222@gmail.com>
2022-09-08 08:13:18 +0200

    fix some bugs

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.set("trust proxy", 1);
 
 app.use(
   rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    windowMs: 15 * 60 * 1000,
+    max: 100,
   })
 );
 
